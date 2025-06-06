5c995db954770ce083a097309a7e143dbade97d4
Thiago Ferreira <thiago.rodrigues@sp.senai.br>
2025-06-06 13:18:44 -0300

    adiciona novos comandos git ao aulagit.js

--- aulagit.js
+++ aulagit.js
@@ -3,3 +3,7 @@
 // git init -> inicia um repositório git local.
 
 // git add . -> adiciona todos os arquivos do diretório atual ao repositório git local.
+
+// git commit -m "mensagem do commit" -> cria um commit com a mensagem especificada.
+
+// git push -> envia as alterações locais para o repositório remoto.
