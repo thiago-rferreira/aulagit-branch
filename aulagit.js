b60518b95ce3af2edb1447d2fa331f1cf6e8ce8d
Thiago Ferreira <thiago.rodrigues@sp.senai.br>
2025-06-06 13:18:02 -0300

    adiciona arquivo aulagit.js com comandos básicos do git

---
+++ aulagit.js
@@ -0,0 +1,5 @@
+// Comandos git -> https://git-scm.com/docs/git
+
+// git init -> inicia um repositório git local.
+
+// git add . -> adiciona todos os arquivos do diretório atual ao repositório git local.
