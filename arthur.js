c4b1dbbbead4833c1b0ff91d775d0d8da69114c1
Arthur Morais <mlkliso4000@gmail.com>
2025-06-06 14:04:43 -0300

    declaracao arthur

---
+++ arthur.js
@@ -0,0 +1,5 @@
+/*
+Desde a primeira que vi o curso confesso que me apaixonei... Talvez não pela programação, por letras coloridas, formatações de computadores
+ou até mesmo apresentações com tempo cronometrado, mas sim, pelos professores incríveis que o curso proporciona para todos seus alunos. Além 
+dessas vantagens, poder fazer trabalhos com meus amigos é extremamente gratificante. É incrível. É maravilhoso. Se eu possuísse inúmeros dicionários Aurélio não teria palavras para descrever minha felicidade!
+*/
